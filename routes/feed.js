555127e9af41a2128a714d9ffeb7ec40c00c0dcd
Bryan C <brybryc.music@gmail.com>
2016-07-31 11:49:43 -0700

    Update

--- routes/feed.js
+++ routes/feed.js
@@ -4,7 +4,7 @@
 var request = require('request');
 module.exports = function(req,res){
     console.log(req.url.slice(6));
-    request("http://api.nytimes.com/svc/news/v3/content/all/"+req.query.category+"/.json?api-key=b22edf5b42404e458938dc21a95a4ed0&"+req.url.slice(6), function(err,data){
+    request("http://api.nytimes.com/svc/news/v3/content/all/"+req.query.category+"/.json?api-key=get-your-own"+req.url.slice(6), function(err,data){
         if(err){
             console.log(err);
         }
